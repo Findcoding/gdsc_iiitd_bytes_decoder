18a0b0be1247006992b2a3dbd002b3077bd07fb7
findcoding <bijendarprasad772244@gmail.com>
2022-12-04 20:07:29 +0530

    all timezone added

--- static/js/home.js
+++ static/js/home.js
@@ -1,34 +1,77 @@
-const date_url = "http://worldtimeapi.org/api/timezone/Asia/Kolkata";
+
+
+const date_url = "http://worldtimeapi.org/api/timezone/";
 
 async function refreshTime() {
 
-    const response = await fetch(date_url);
-    const data = await response.json();
+    let city = document.getElementById("city").value;
 
-    console.log(data);
+    if(city != 'none') {
 
-    const dateTimeInParts = data.datetime.split("T");
-    console.log(dateTimeInParts)
+        document.getElementById('datatime_card').style.display = "block";
 
-    const arr1 = dateTimeInParts[0].split("-");
-    // console.log(arr1);
+        const response = await fetch(date_url + city);
+        const data = await response.json();
 
-    const arr2 = dateTimeInParts[1].split(":");
-    // console.log(arr2);
+        const dateTimeInParts = data.datetime.split("T");
 
-    var second = arr2[2].split(".");
+        const arr1 = dateTimeInParts[0].split("-");
+        // console.log(arr1);
 
-    var zone = arr2[0] >= 12 ? 'PM' : 'AM';
+        const arr2 = dateTimeInParts[1].split(":");
+        // console.log(arr2);
 
-    document.getElementById("hour").textContent = arr2[0] + ":";
-    document.getElementById("min").textContent = arr2[1] + ":";
-    document.getElementById("sec").textContent = second[0] + " ";
-    document.getElementById("zone").textContent = zone;
+        var h = arr2[0] % 12;
+        var hour =  h < 10 ? '0'+h : h;
 
-    document.getElementById("date").textContent = arr1[2] + "-" + arr1[1] + "-" + arr1[0];
+        var second = arr2[2].split(".");
+        var zone = arr2[0] >= 12 ? 'PM' : 'AM';
+
+        document.getElementById("hour").textContent = hour + ":";
+        document.getElementById("min").textContent = arr2[1] + ":";
+        document.getElementById("sec").textContent = second[0] + " ";
+        document.getElementById("zone").textContent = zone;
+
+        document.getElementById("date").textContent = arr1[2] + "-" + arr1[1] + "-" + arr1[0];
+
+    } else {
+        document.getElementById('datatime_card').style.display = "none";
+    }
 
 }
 
-// refreshTime()
+
 
 setInterval(refreshTime, 1000);
+
+
+
+const zone_url = "http://worldtimeapi.org/api/timezone";
+
+async function zone() {
+
+    const response = await fetch(zone_url);
+    const data = await response.json();
+
+    let select = document.getElementById("city");
+    for (let i = 0; i < data.length; i++) {
+        let optn = data[i];
+        let el = document.createElement("option");
+
+        let txt = data[i].split("/")
+        el.textContent = txt[1];
+
+        el.value = optn;
+        select.appendChild(el);
+    }
+
+}
+
+zone()
+
+
+
+function Time() {
+    // refreshTime()
+    setInterval(refreshTime, 1000);
+}
