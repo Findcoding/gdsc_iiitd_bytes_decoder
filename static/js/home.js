9857c7385dcff56e7a169828e18b8e4c36002b29
findcoding <bijendarprasad772244@gmail.com>
2022-12-04 19:27:50 +0530

    zone value added in time

--- static/js/home.js
+++ static/js/home.js
@@ -1,33 +1,31 @@
-console.log('hell');
-
-
-
-const date_url = "http://worldtimeapi.org/api/timezone/America/New_York";
+const date_url = "http://worldtimeapi.org/api/timezone/Asia/Kolkata";
 
 async function refreshTime() {
 
     const response = await fetch(date_url);
-        const data = await response.json();
+    const data = await response.json();
 
-        console.log(data);
+    console.log(data);
 
-        const dateTimeInParts = data.datetime.split("T");
-        console.log(dateTimeInParts)
+    const dateTimeInParts = data.datetime.split("T");
+    console.log(dateTimeInParts)
 
-        const arr1 = dateTimeInParts[0].split("-");
-        console.log(arr1);
+    const arr1 = dateTimeInParts[0].split("-");
+    // console.log(arr1);
 
-        const arr2 = dateTimeInParts[1].split(":");
-        console.log(arr2);
+    const arr2 = dateTimeInParts[1].split(":");
+    // console.log(arr2);
 
-        var second = arr2[2].split(".");
+    var second = arr2[2].split(".");
 
-        document.getElementById("hour").textContent = arr2[0] + ":";
-        document.getElementById("min").textContent = arr2[1] + ":";
-        document.getElementById("sec").textContent = second[0] + " ";
-        document.getElementById("zone").textContent = "AM";
+    var zone = arr2[0] >= 12 ? 'PM' : 'AM';
 
-        document.getElementById("date").textContent = arr1[2] + "-" + arr1[1] + "-" + arr1[0];
+    document.getElementById("hour").textContent = arr2[0] + ":";
+    document.getElementById("min").textContent = arr2[1] + ":";
+    document.getElementById("sec").textContent = second[0] + " ";
+    document.getElementById("zone").textContent = zone;
+
+    document.getElementById("date").textContent = arr1[2] + "-" + arr1[1] + "-" + arr1[0];
 
 }
 
